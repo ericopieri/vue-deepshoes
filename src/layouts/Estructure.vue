85dc45346acf5c3d28d86d14629f269f959de253
Gustavo Veio <guukubiack@gmail.com>
2022-06-21 03:23:15 -0300

    primeiro commit

---
+++ src/layouts/Estructure.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="estrutura">
+        <header id="header-page">
+                <div id="logo">LOGO</div>
+                <nav id="nav-menu">
+                    <ul id="nav-list">
+                        <li class="nav-item-menu"><a href="" class="nav-list-link"><i class="fa-solid fa-cart-shopping"></i></a></li>
+                        <li class="nav-item-menu"><a href="" class="nav-list-link"><i class="fa-solid fa-user"></i></a></li>
+                        <li class="nav-item-menu"><a href="" class="nav-list-link"><i class="fa-solid fa-right-from-bracket"></i></a></li>
+                    </ul>
+                </nav>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
