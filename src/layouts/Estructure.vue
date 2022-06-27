a1bad1997c571f6da9e7a13e8675167d9389e502
eronzin13 <erontiago6@gmail.com>
2022-06-27 11:57:35 -0300

    27/06/22

--- src/layouts/Estructure.vue
+++ src/layouts/Estructure.vue
@@ -2,11 +2,11 @@
     <div id="estrutura">
         <header id="header-page">
                 <div id="logo">LOGO</div>
+                <input type="text" id="filtro-home" placeholder="O que você está procurando?">
                 <nav id="nav-menu">
                     <ul id="nav-list">
-                        <li class="nav-item-menu"><a href="" class="nav-list-link"><i class="fa-solid fa-cart-shopping"></i></a></li>
-                        <li class="nav-item-menu"><a href="" class="nav-list-link"><i class="fa-solid fa-user"></i></a></li>
-                        <li class="nav-item-menu"><a href="" class="nav-list-link"><i class="fa-solid fa-right-from-bracket"></i></a></li>
+                        <li class="nav-item-menu"><a href="" class="nav-list-link"><i class="fa-solid fa-cart-shopping"></i><span class="nav-item-span">Carrinho</span></a></li>
+                        <li class="nav-item-menu"><a href="" class="nav-list-link"><i class="fa-solid fa-user"></i><span class="nav-item-span">Logar</span></a></li>
                     </ul>
                 </nav>
         </header>
