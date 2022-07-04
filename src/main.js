073255cd79d82c2317b6127fad3a75f583f93987
KaueDeMoura <kaueoenningdmoura@gmail.com>
2022-07-04 11:19:23 -0300

    alteracoes slides

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import './assets/style.css'
+import './assets/css/style.css'
 import VueCarousel from 'vue-carousel';
 
 Vue.use(VueCarousel);
