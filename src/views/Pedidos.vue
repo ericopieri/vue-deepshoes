81b4c6cf06c3b9f46b7bc068d0e1816aa6fcf812
Erico Pieri <erico.pieri@gmail.com>
2022-10-15 22:12:56 -0300

    rota para carrinho

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,7 +1,29 @@
-<template><div>Pedidos!</div></template>
+<template>
+  <div>
+    <h2 id="titulo-pedidos">Seus Pedidos já feitos:</h2>
+    <div class="container-pedidos"></div>
+  </div>
+</template>
 
 <script>
-export default {};
+import { api } from "../axios/index";
+
+export default {
+  data() {
+    return {
+      pedidos: [],
+    };
+  },
+  mounted() {
+    this.getPedidos();
+  },
+  methods: {
+    async getPedidos() {
+      this.pedidos = await api.get("/api/pedidos/");
+      console.log(this.pedidos);
+    },
+  },
+};
 </script>
 
 <style>
