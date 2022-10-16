356344b4c822492154581d7ebef3ddfbe6dcaa7e
Erico Pieri <erico.pieri@gmail.com>
2022-10-15 22:51:33 -0300

    página de produto (quase finalizada)

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <h2 id="titulo-pedidos">Seus Pedidos já feitos:</h2>
-    <div class="container-pedidos"></div>
+    <div id="container-pedidos">
+      <div class="box-pedido" v-for="(pedido, index) of pedidos" :key="index">
+        <div class="info-pedido">
+          <div class="quantidade-itens">
+            <b>Itens:</b> {{ pedido.itens.length }}
+          </div>
+          <div class="datas">
+            <div><b>Data do Pedido:</b> {{ pedido.data_pedido }}</div>
+            <div><b>Data de Entrega:</b> {{ pedido.data_entrega }}</div>
+          </div>
+          <div class="preco-pedido">
+            <b>Preço:</b> R$ {{ pedido.preco_total }}
+          </div>
+        </div>
+        <button class="button-pedido">+ Detalhes</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,8 +35,8 @@ export default {
   },
   methods: {
     async getPedidos() {
-      this.pedidos = await api.get("/api/pedidos/");
-      console.log(this.pedidos);
+      const { data } = await api.get("/api/pedidos/");
+      this.pedidos = data;
     },
   },
 };
